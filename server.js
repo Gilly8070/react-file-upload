751d1b85716d32ad4a176cb973e30f3dd2f4766b
Gulrez <sidd5312@gmailcom>
2021-03-07 05:36:15 +0530

    initial changes

--- server.js
+++ server.js
@@ -23,4 +23,18 @@ app.post('/upload', (req, res) => {
     });
     });
 
-    app.listen(5000, () => console.log('Server Started...'));
+    // Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client",  "build", "index.html"))
+    );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+    // app.listen(5000, () => console.log('Server Started...'));
