f52e533318e476248983d392ad36ed5234854d35
Gulrez <sidd5312@gmailcom>
2021-03-07 05:59:43 +0530

    procfile removed

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.post('/upload', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-    // app.listen(5000, () => console.log('Server Started...'));
+    app.listen(5000, () => console.log('Server Started...'));
