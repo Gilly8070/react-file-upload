855b1f0ac163fe7ebdb547dc08bba415fa1a8714
Gulrez <sidd5312@gmailcom>
2021-03-07 05:57:42 +0530

    procfile removed

--- server.js
+++ server.js
@@ -5,33 +5,33 @@ const app = express();
 
 app.use(fileUpload());
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-    app.use(express.static("client/build"));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client",  "build", "index.html"))
-    );
-}
+//     app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client",  "build", "index.html"))
+//     );
+// }
 
-// // Upload Endpoint
-// app.post('/upload', (req, res) => {
-//     if (req.files === null) {
-//         return res.status(400).json({ msg: 'No file uploaded' });
-//     }
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+    if (req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded' });
+    }
 
-//     const file = req.files.file;
+    const file = req.files.file;
 
-//     file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-//         if (err) {
-//         console.error(err);
-//         return res.status(500).send(err);
-//         }
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+        }
 
-//         res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-//     });
-//     });
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
+    });
 
 
 
