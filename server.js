6c96d4ca87e76d4cd5f8cea6d0eb6fad4cfce123
Gulrez <sidd5312@gmailcom>
2021-03-07 06:41:20 +0530

    removed build

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.post('/upload', (req, res) => {
     });
     });
 
-    
-app.use(express.static(path.join(__dirname, 'build')));
+
+// app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 
