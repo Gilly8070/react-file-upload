b0f2621d7b8bf101612221d1f7e66c3d2e80130c
Gulrez <sidd5312@gmailcom>
2021-03-07 06:34:39 +0530

    procfile removed

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ app.post('/upload', (req, res) => {
     });
     });
 
+    
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 
 // const PORT = process.env.PORT || 5000;
